e1b0d5a330f29947de5e013f88844a314ea74bcf
byronklep <byronklep@gmail.com>
2021-05-08 19:04:54 -0500

    Footer content.

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,13 +3,94 @@ import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
   return (
-    <footer className='bg-dark'>
-      <Container>
-        <Row>
-          <Col className='text-center text-light py-3'>Copyright &copy; Art Store</Col>
-        </Row>
-      </Container>
-    </footer>
+    // <footer className='bg-dark'>
+    //   <Container>
+    //     <Row>
+    //       <Col className='text-center text-light py-3'>Copyright &copy; Art Store</Col>
+    //     </Row>
+    //   </Container>
+    // </footer>
+    <section className="">
+      <footer className="bg-dark text-white text-center">
+        <div className="container p-4">
+          <div className="row">
+            <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+              <h5 className="text-uppercase">Art Store</h5>
+
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
+                atque ea quis molestias. Fugiat pariatur maxime quis culpa
+                corporis vitae repudiandae aliquam voluptatem veniam, est atque
+                cumque eum delectus sint!
+              </p>
+            </div>
+
+            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+              <h5 className="text-uppercase">Links</h5>
+
+              <ul className="list-unstyled mb-0">
+                <li>
+                  <a href="#!" className="text-white">
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">
+                    Link 4
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+              <h5 className="text-uppercase mb-0">Links</h5>
+
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#!" className="text-white">
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">
+                    Link 4
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="text-center p-3"
+          style={{"backgroundColor": "rgba(0, 0, 0, 0.2)"}}>
+          Copyright &copy; 
+          <a className="text-white" href="/">
+          Art Store
+          </a>
+        </div>
+      </footer>
+    </section>
   )
 }
 
